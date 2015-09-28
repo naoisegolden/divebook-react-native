09cc844c0d938e7ad56d0ef6c3a465441ca8672e
Naoise Golden <naoise.golden@xing.com>
2015-09-28 18:45:59 +0200

    Fetch data from Parse instead of mocked

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,13 @@ var MOCKED_DIVESITE_DATA = [
   {name: 'Name 03', location: 'Illes Medes, Girona, Spain'},
 ];
 
+var PARSE_APPLICATION_ID = 'Y31uHUHsSOcmfEjLPt2uvT2Qmc53EuX2JCB0iHsK';
+var PARSE_JAVASCRIPT_KEY = 'PUA1mxzlF8dA6DgQQlK5UhO3zHDOH3tr7mp2usNu';
+
 var React = require('react-native');
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+
 var {
   AppRegistry,
   ListView,
@@ -15,37 +21,63 @@ var {
   View,
 } = React;
 
+Parse.initialize(
+  PARSE_APPLICATION_ID,
+  PARSE_JAVASCRIPT_KEY,
+);
+
 var Divebook = React.createClass({
+  mixins: [ParseReact.Mixin], // Enable query subscriptions
+
+  observe: function() {
+    // The results will be available at this.data.divesites
+    return {
+      divesites: (new Parse.Query('divesite')).ascending('createdAt')
+    };
+  },
   getInitialState: function () {
     return {
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
-      }).cloneWithRows(MOCKED_DIVESITE_DATA)
+      }),
+      loading: true,
     };
   },
-  componentDidMount: function() {
-    this.fetchData();
+  componentDidUpdate: function(prevProps, prevState) {
+    if (prevState.loading && (this.pendingQueries().length == 0)) {
+      this.setState({ loading: false });
+    }
   },
-  fetchData: function () {
-   // Return mocked data for now
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(MOCKED_DIVESITE_DATA)
-    });
-  },
+
   render: function() {
+    if (this.state.loading) {
+      return this.renderLoadingView();
+    }
+
     return (
       <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderDivesite}
+        dataSource={this.state.dataSource.cloneWithRows(this.data.divesites)}
+        renderRow={this.renderDivesiteView}
         style={styles.listView}
       />
     );
   },
-  renderDivesite: function(divesite) {
+
+  renderDivesiteView: function(divesite) {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Dive Site: {divesite.name}</Text>
+        <Text style={styles.subtitle}>Address: {divesite.address}</Text>
         <Text style={styles.subtitle}>Location: {divesite.location}</Text>
+      </View>
+    );
+  },
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading...
+        </Text>
       </View>
     );
   },
