f673d4027fdf333c5312c607ebe44fb6950859e0
Naoise Golden <naoise.golden@xing.com>
2015-09-28 17:08:51 +0200

    Make build work in physical iPhone

--- ios/Divebook/AppDelegate.m
+++ ios/Divebook/AppDelegate.m
@@ -31,7 +31,8 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+  // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://172.27.26.55:8081/index.ios.bundle?platform=ios&dev=false"];
 
   /**
    * OPTION 2
