dd55f361f4eab7cc487ea6fb1a2978fd85e257bd
Naoise Golden <naoise.golden@xing.com>
2015-09-28 17:30:41 +0200

    List view with mocked data

--- index.ios.js
+++ index.ios.js
@@ -1,34 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
+
+var MOCKED_DIVESITE_DATA = [
+  {name: 'Name 01', location: 'Tossa de Mar, Girona, Spain'},
+  {name: 'Name 02', location: 'Puerto del Carmen, Lanzarote, Spain'},
+  {name: 'Name 03', location: 'Illes Medes, Girona, Spain'},
+];
 
 var React = require('react-native');
 var {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
 var Divebook = React.createClass({
+  getInitialState: function () {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }).cloneWithRows(MOCKED_DIVESITE_DATA)
+    };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function () {
+   // Return mocked data for now
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(MOCKED_DIVESITE_DATA)
+    });
+  },
   render: function() {
     return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderDivesite}
+        style={styles.listView}
+      />
+    );
+  },
+  renderDivesite: function(divesite) {
+    return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.title}>Dive Site: {divesite.name}</Text>
+        <Text style={styles.subtitle}>Location: {divesite.location}</Text>
       </View>
     );
-  }
+  },
 });
 
 var styles = StyleSheet.create({
@@ -38,15 +58,19 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  subtitle: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
